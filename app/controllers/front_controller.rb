a7e9b02473d1f44cb305caa0d907d32b1c5f059a
byendan <byendanb@gmail.com>
2016-03-07 17:46:02 -0800

    About text on front

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -2,6 +2,7 @@ class FrontController < ApplicationController
   skip_before_action :authorize
   def show
     @bubbles = Bubble.all
+    @about_text = TextContent.find_by(parent: "about")
   end
 
 end

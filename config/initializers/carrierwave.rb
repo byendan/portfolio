a4fa9b4ccfa4f8eef4888775b79f5be18c083515
byendan <byendanb@gmail.com>
2016-03-20 08:54:46 -0700

    More reconfig

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,19 +1,14 @@
 CarrierWave.configure do |config|
 
 
-  config.root = Rails.root.join('tmp') # adding these...
-  config.cache_dir = 'carrierwave' # ...two lines
+
   config.fog_credentials = {
     :provider               => 'Rackspace',
     :rackspace_username     => ENV['RACKSPACE_USERNAME'],
     :rackspace_api_key      => ENV['RACKSPACE_API_KEY'],
-    :rackspace_servicenet   => Rails.env.production?,
-    :rackspace_region       => ENV['RACKSPACE_REGION'],
-    :persistent => false
+    :rackspace_region       => ENV['RACKSPACE_REGION']
   }
   config.fog_directory = 'portfolio'
-  config.ignore_integrity_errors = false
-  config.ignore_processing_errors = false
-  config.ignore_download_errors = false
+
 
 end
